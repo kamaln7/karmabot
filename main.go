0cd2bb17121a0bb99208d33387140857e6dbd2df
Kamal Nasser <hello@kamal.io>
2016-11-07 19:05:22 +0200

    implement reactji-based karma operations (#22)
    this fixes #4

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ type Slack struct {
 // Config contains all the necessary configs for karmabot.
 type Config struct {
 	Slack                       *Slack
-	Debug, Motivate             bool
+	Debug, Motivate, Reactji    bool
 	MaxPoints, LeaderboardLimit int
 	Log                         *log.Log
 	UI                          ui.Provider
@@ -63,6 +63,10 @@ func (b *Bot) Listen() {
 		select {
 		case msg := <-b.Config.Slack.RTM.IncomingEvents:
 			switch ev := msg.Data.(type) {
+			case *slack.ReactionAddedEvent:
+				go b.handleReactionAddedEvent(msg.Data.(*slack.ReactionAddedEvent))
+			case *slack.ReactionRemovedEvent:
+				go b.handleReactionRemovedEvent(msg.Data.(*slack.ReactionRemovedEvent))
 			case *slack.MessageEvent:
 				go b.handleMessageEvent(msg.Data.(*slack.MessageEvent))
 			case *slack.ConnectedEvent:
@@ -87,6 +91,17 @@ func (b *Bot) SendMessage(message, channel string) {
 	b.Config.Slack.RTM.SendMessage(b.Config.Slack.RTM.NewOutgoingMessage(message, channel))
 }
 
+// DMUser sends a message directly to a Slack user.
+func (b *Bot) DMUser(message, user string) {
+	_, _, channel, err := b.Config.Slack.Bot.OpenIMChannel(user)
+	if err != nil {
+		b.Config.Log.Err(err).KV("user", user).Error("could not open IM channel with user")
+		return
+	}
+
+	b.Config.Slack.RTM.SendMessage(b.Config.Slack.RTM.NewOutgoingMessage(message, channel))
+}
+
 func (b *Bot) handleError(err error, channel string) bool {
 	if err == nil {
 		return false
@@ -94,15 +109,95 @@ func (b *Bot) handleError(err error, channel string) bool {
 
 	fmt.Printf("%#v\n%T\n", err, err)
 	b.Config.Log.Err(err).Error("error")
-	var message string
-	if b.Config.Debug {
-		message = err.Error()
-	} else {
-		message = "an error has occurred."
+	if channel != "" {
+		var message string
+		if b.Config.Debug {
+			message = err.Error()
+		} else {
+			message = "an error has occurred."
+		}
+
+		b.SendMessage(message, channel)
 	}
 
-	b.SendMessage(message, channel)
 	return true
+}
+
+func (b *Bot) handleReactionAddedEvent(ev *slack.ReactionAddedEvent) {
+	if !b.Config.Reactji {
+		return
+	}
+
+	var (
+		points int
+		reason string
+	)
+	switch ev.Reaction {
+	case "+1":
+		points = +1
+		reason = "adding a :thumbsup: reactji"
+	case "-1":
+		points = -1
+		reason = "adding a :thumbsdown: reactji"
+	default:
+		return
+	}
+
+	b.handleReactionEvent(ev.User, ev.ItemUser, reason, points)
+}
+
+func (b *Bot) handleReactionRemovedEvent(ev *slack.ReactionRemovedEvent) {
+	if !b.Config.Reactji {
+		return
+	}
+
+	var (
+		points int
+		reason string
+	)
+	switch ev.Reaction {
+	case "+1":
+		points = -1
+		reason = "removing a :thumbsup: reactji"
+	case "-1":
+		points = 1
+		reason = "removing a :thumbsdown: reactji"
+	default:
+		return
+	}
+
+	b.handleReactionEvent(ev.User, ev.ItemUser, reason, points)
+}
+
+func (b *Bot) handleReactionEvent(fromID, toID, reason string, points int) {
+	from, err := b.getUserNameByID(fromID)
+	if b.handleError(err, "") {
+		return
+	}
+	to, err := b.getUserNameByID(toID)
+	if b.handleError(err, "") {
+		return
+	}
+	from, to = strings.ToLower(from), strings.ToLower(to)
+
+	record := &database.Points{
+		From:   from,
+		To:     to,
+		Points: points,
+		Reason: reason,
+	}
+
+	err = b.Config.DB.InsertPoints(record)
+	if b.handleError(err, "") {
+		return
+	}
+
+	pointsMsg, err := b.getUserPointsMessage(to, reason, points)
+	if b.handleError(err, "") {
+		return
+	}
+
+	b.DMUser(pointsMsg, fromID)
 }
 
 func (b *Bot) handleMessageEvent(ev *slack.MessageEvent) {
@@ -194,12 +289,21 @@ func (b *Bot) givePoints(ev *slack.MessageEvent) {
 		return
 	}
 
-	user, err := b.Config.DB.GetUser(to)
+	pointsMsg, err := b.getUserPointsMessage(to, reason, points)
 	if b.handleError(err, ev.Channel) {
 		return
 	}
 
-	text := fmt.Sprintf("%s == %d (", to, user.Points)
+	b.SendMessage(pointsMsg, ev.Channel)
+}
+
+func (b *Bot) getUserPointsMessage(name, reason string, points int) (string, error) {
+	user, err := b.Config.DB.GetUser(name)
+	if err != nil {
+		return "", err
+	}
+
+	text := fmt.Sprintf("%s == %d (", name, user.Points)
 
 	if points > 0 {
 		text += "+"
@@ -211,7 +315,7 @@ func (b *Bot) givePoints(ev *slack.MessageEvent) {
 	}
 	text += ")"
 
-	b.SendMessage(text, ev.Channel)
+	return text, nil
 }
 
 func (b *Bot) printLeaderboard(ev *slack.MessageEvent) {
