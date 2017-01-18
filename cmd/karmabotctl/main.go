1158d06371f619d59bba19a48a6896e372a9fac6
Kamal Nasser <hello@kamal.io>
2017-01-18 16:31:41 +0200

    WIP

--- cmd/karmabotctl/main.go
+++ cmd/karmabotctl/main.go
@@ -47,46 +47,83 @@ func main() {
 		Usage: "the default amount of users to list in the leaderboard",
 	}
 
+	// webui
+
+	webuiCommands := []cli.Command{
+		{
+			Name:  "totp",
+			Usage: "get a URL with a valid TOTP token",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "totp",
+					Usage: "totp key",
+				},
+			},
+			Action: mktotp,
+		},
+		{
+			Name:  "serve",
+			Usage: "start a webserver",
+			Flags: []cli.Flag{
+				dbpath,
+				debug,
+				leaderboardlimit,
+				cli.StringFlag{
+					Name:  "totp",
+					Usage: "totp key",
+				},
+				cli.StringFlag{
+					Name:  "path",
+					Usage: "path to web UI files",
+				},
+				cli.StringFlag{
+					Name:  "listenaddr",
+					Usage: "address to listen and serve the web ui on",
+				},
+				cli.StringFlag{
+					Name:  "url",
+					Usage: "url address for accessing the web ui",
+				},
+			},
+			Action: serve,
+		},
+	}
+
+	// karma
+
+	karmaCommands := []cli.Command{
+		{
+			Name:  "add",
+			Usage: "add karma to a user",
+			Flags: []cli.Flag{
+				dbpath,
+				cli.StringFlag{
+					Name: "from",
+				},
+				cli.StringFlag{
+					Name: "to",
+				},
+				cli.StringFlag{
+					Name: "reason",
+				},
+				cli.IntFlag{
+					Name: "points",
+				},
+			},
+			Action: addKarma,
+		},
+	}
+
+	// main app
+
 	app.Commands = []cli.Command{
 		{
-			Name: "webui",
-			Subcommands: []cli.Command{{
-				Name:  "totp",
-				Usage: "get a URL with a valid TOTP token",
-				Flags: []cli.Flag{
-					cli.StringFlag{
-						Name:  "totp",
-						Usage: "totp key",
-					},
-				},
-				Action: mktotp,
-			},
-				{
-					Name:  "serve",
-					Usage: "start a webserver",
-					Flags: []cli.Flag{
-						dbpath,
-						debug,
-						leaderboardlimit,
-						cli.StringFlag{
-							Name:  "totp",
-							Usage: "totp key",
-						},
-						cli.StringFlag{
-							Name:  "path",
-							Usage: "path to web UI files",
-						},
-						cli.StringFlag{
-							Name:  "listenaddr",
-							Usage: "address to listen and serve the web ui on",
-						},
-						cli.StringFlag{
-							Name:  "url",
-							Usage: "url address for accessing the web ui",
-						},
-					},
-					Action: serve,
-				}},
+			Name:        "karma",
+			Subcommands: karmaCommands,
+		},
+		{
+			Name:        "webui",
+			Subcommands: webuiCommands,
 		},
 	}
 
@@ -135,6 +172,38 @@ func mktotp(c *cli.Context) error {
 	}
 
 	ll.KV("token", token).Info("generated token")
+
+	return nil
+}
+
+func addKarma(c *cli.Context) error {
+	var (
+		db     = getDB(c.String("dbpath"))
+		from   = c.String("from")
+		to     = c.String("to")
+		reason = c.String("reason")
+		points = c.Int("points")
+	)
+
+	if from == "" || to == "" {
+		ll.Fatal("please pass valid users to the `to` and `from` options")
+	}
+
+	if points == 0 {
+		ll.Fatal("you may not add 0 points to a user")
+	}
+
+	record := &database.Points{
+		From:   from,
+		To:     to,
+		Reason: reason,
+		Points: points,
+	}
+
+	err := db.InsertPoints(record)
+	if err != nil {
+		ll.Err(err).Fatal("could not insert record")
+	}
 
 	return nil
 }
