85c1b20f9de1cbeab85d8e41c155051fab3a37e2
Luke Granger-Brown <git@lukegb.com>
2017-07-31 22:29:47 +0100

    Add stub chat service and database layers

---
+++ chat_service_test.go
@@ -0,0 +1,43 @@
+package karmabot
+
+import "github.com/nlopes/slack"
+
+type TestChatService struct {
+	IncomingEvents chan slack.RTMEvent
+
+	SentMessages []*slack.OutgoingMessage
+	id           int
+}
+
+func newTestChatService() ChatService {
+	return &TestChatService{}
+}
+
+func (t *TestChatService) IncomingEventsChan() chan slack.RTMEvent {
+	return t.IncomingEvents
+}
+
+func (t *TestChatService) OpenIMChannel(user string) (bool, bool, string, error) {
+	return true, true, user, nil
+}
+
+func (t *TestChatService) GetUserInfo(user string) (*slack.User, error) {
+	return &slack.User{
+		ID:   user,
+		Name: user,
+	}, nil
+}
+
+func (t *TestChatService) NewOutgoingMessage(text string, channel string) *slack.OutgoingMessage {
+	t.id++
+	return &slack.OutgoingMessage{
+		ID:      t.id,
+		Type:    "message",
+		Channel: channel,
+		Text:    text,
+	}
+}
+
+func (t *TestChatService) SendMessage(m *slack.OutgoingMessage) {
+	t.SentMessages = append(t.SentMessages, m)
+}
