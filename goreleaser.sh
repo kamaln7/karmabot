cdde60290cb67b680cf9f9b512e68c88bbda6915
Kamal Nasser <hello@kamal.io>
2019-10-11 18:22:09 +0300

    update build env + goreleaser config

--- goreleaser.sh
+++ goreleaser.sh
@@ -7,5 +7,6 @@ docker run \
 	-v $(pwd):/go/src/github.com/kamaln7/karmabot \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $HOME/.config/goreleaser/github_token:/root/.config/goreleaser/github_token \
-    -v $HOME/.docker/config.json:/root/.docker/config.json \
-	karmabot-goreleaser $@
+    	-v $HOME/.docker/config.json:/root/.docker/config.json \
+	-w /go/src/github.com/kamaln7/karmabot \
+	karmabot-build-env $@
